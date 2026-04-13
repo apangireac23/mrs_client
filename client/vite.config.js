340fede22e157853d1401fb3e6a158f39d563f46
Arnav Apangire <apangireac23@outlook.com>
2026-04-13 11:59:06 +0530

    Fix: Add eslint disable comments for process.env in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,17 @@ import react from '@vitejs/plugin-react'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 // https://vite.dev/config/
+// eslint-disable-next-line no-undef
 export default defineConfig({
   plugins: [
     react(),
+    // eslint-disable-next-line no-undef
     ...(process.env.SENTRY_AUTH_TOKEN
       ? [
           sentryVitePlugin({
             org: 'your-org',
             project: 'your-project',
+            // eslint-disable-next-line no-undef
             authToken: process.env.SENTRY_AUTH_TOKEN,
           }),
         ]
